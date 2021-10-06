b07d6c75f012e5fdc8288cbe0dad88e38a46e034
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-06 11:20:30 -0400

    fix potential issue with strlen and added strcpy

--- hw-08.c
+++ hw-08.c
@@ -4,6 +4,16 @@ int mystrlen(char *s){
     int result = 0;
     while (s){
         result++;
+        s++;
     }
     return result;
+}
+
+char * mystrcpy(char *dest, char *source){
+    while (source){
+        dest = source;
+        dest++;
+        source++;
+    }
+    return *dest;
 }
