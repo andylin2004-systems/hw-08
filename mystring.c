f0035fb71efd0c1d3b97615c1827555cb43379da
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-10-07 11:30:05 -0400

    fix issue where strcmp failed to increment and handle case when one string is longer than another string

--- mystring.c
+++ mystring.c
@@ -37,15 +37,21 @@ char * mystrncat(char *dest, char *source, int n){
 int mystrcmp( char *s1, char *s2 ){
     while (*s1 && *s2)
     {
-        if (*s1 != *s2){
-            if (*s1 > *s2){
-                return 1;
-            }else{
-                return -1;
-            }
+        if (*s1 > *s2){
+            return 1;
+        }else if (*s1 < *s2){
+            return -1;
         }
+        s1++;
+        s2++;
     }
-    return 0;
+    if (*s1 == *s2){
+        return 0;
+    }else if (*s1 == '\0'){
+        return 1;
+    }else{
+        return -1;
+    }
 }
 
 char * mystrchr(char *s, char c){
